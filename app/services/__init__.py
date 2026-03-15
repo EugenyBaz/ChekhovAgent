b858e27031dd43f91b5313c6b5b8537a51a157d0
Bazavod <eugeny.bazavod@list.ru>
2026-03-15 21:02:59 +0300

    fixed mistakes by lint

--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.config import settings
-
-if settings.USE_MOCK_LLM:
-    from app.services.answer_service_mock import LLMServiceMock as LLMService
-else:
-    from app.services.answer_service import LLMService
